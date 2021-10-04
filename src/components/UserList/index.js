fdc64781384381669d3f38ad2870326ef819d10e
Ditobaskoro <dito.anwar@yahoo.com>
2021-10-04 10:05:08 +0700

    Add delete user function

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -13,35 +13,49 @@ const USER_LIST_QUERY = gql`
       name
     }
   }
-`;
+`
 
 const DELETE_USER_MUTATION = gql`
-  mutation deletUserMutation(id: ID!) {
+  mutation deletUserMutation($id: ID!) {
     deleteUser(id: $id)
   }
-`;
+`
 
 const UserList = () => {
   const { loading, error, data } = useQuery(USER_LIST_QUERY)
-  const [deletUser, { data, loading, error }] = useMutation(DELETE_USER_MUTATION);
+
+  const [onDeleteUser, { mutationLoading, mutationError }] = useMutation(DELETE_USER_MUTATION, {
+    refetchQueries: [
+      { query: USER_LIST_QUERY }
+    ]
+  })
 
   const [shouldShowDelete, setShouldShowDelete] = useState(false)
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  const renderUsers = () => {
+    if (loading || mutationLoading) return <p>Loading...</p>
+    if (error || mutationError) return <p>Error :(</p>
 
-  return  (
-    <ListContainer>
-      {data.users.map(({ id, name }) => (
+    return data.users.map(({ id, name }) => (
         <User 
           key={id}
           onMouseEnter={() => setShouldShowDelete(true)}
           onMouseLeave={() => setShouldShowDelete(false)}
         >
           {name}
-          {shouldShowDelete && <div onClick={}><CloseIcon size="24" /></div>}
+          {shouldShowDelete && ( 
+            <div onClick={() => onDeleteUser({ variables: { id } })}>
+              <CloseIcon size='24' />
+            </div>
+          )}
         </User>
-      ))}
+      )
+    )
+  }
+
+  return  (
+    <ListContainer>
+      {renderUsers()}
     </ListContainer>
   )
 }
