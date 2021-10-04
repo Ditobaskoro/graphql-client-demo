f5d4567b1fdca30af719cab90da6206a684c76b0
Ditobaskoro <dito.anwar@yahoo.com>
2021-10-04 07:32:49 +0700

    Init client component

---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+/**
+ * Home Component
+ *
+ */
+
+const Home = () => {
+  return (
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+    </div>
+  )
+}
+
+export default Home
